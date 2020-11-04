4029ae74a69f9f574261578e477cdcc6b64848ee
notynote <32334875+notynote@users.noreply.github.com>
2020-11-04 19:33:54 +0700

    Initial Commit

---
+++ RollADice-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RollADice-SwiftUI
+//
+//  Created by Pathompong Phongsaporamut on 4/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollADice-SwiftUI/RollADice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollADice_SwiftUIApp.swift
+//  RollADice-SwiftUI
+//
+//  Created by Pathompong Phongsaporamut on 4/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RollADice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
