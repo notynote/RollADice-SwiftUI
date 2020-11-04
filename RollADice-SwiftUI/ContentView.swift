7c1db6f97aa6cf8731c4a5f28a401788160f25ff
notynote <32334875+notynote@users.noreply.github.com>
2020-11-04 19:49:37 +0700

    Implement UI Layout

--- RollADice-SwiftUI/ContentView.swift
+++ RollADice-SwiftUI/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: 1)
+                    DiceView(number: 2)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+//MARK: - DiceView
+struct DiceView: View {
+    
+    let number: Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
